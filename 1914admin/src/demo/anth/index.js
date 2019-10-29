99ee3d09e051e0817404ebd2bf079615dc16b56a
wangyangya <1642540778@qq.com>
2019-10-29 17:50:52 +0800

    zc init

---
+++ 1914admin/src/demo/anth/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Button} from 'antd'
+function Btn() {
+    return(
+        <div className="less">
+          <h3>111</h3>
+          <Button>呵呵哒</Button>
+        </div>
+    )
+
+
+}
+
+export default Btn;
---
+++ 1914admin/src/demo/less/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './index.less'
+function Less() {
+    return(
+        <div className="less">
+            <div className="son1">
+
+            </div>
+            <div className="son2">
+
+            </div>
+        </div>
+    )
+
+
+}
+
+export default Less;
